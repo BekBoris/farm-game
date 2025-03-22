ab7c7bf00a90d549912e5362d090d0121d8cdc91
BekBoris <38814687+BekBoris@users.noreply.github.com>
2025-03-22 13:26:05 +0400

    Initial commit

---
+++ src/Experience/World/Farm.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three'
+import Experience from '../Experience.js'
+
+export default class Farm
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        this.time = this.experience.time
+        this.debug = this.experience.debug
+
+        this.resource = this.resources.items.farmModel
+
+        this.create()
+    }
+
+    create()
+    {
+        this.model = this.resource.scene
+        this.model.scale.set(0.02, 0.02, 0.02)
+        this.scene.add(this.model)
+    }
+
+}
---
+++ src/Experience/World/World.js
@@ -0,0 +1,29 @@
+import Experience from '../Experience.js'
+import Environment from './Environment.js'
+import Farm from './Farm.js'
+import Fox from './Fox.js'
+
+export default class World
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+        // Wait for resources
+        this.resources.on('ready', () =>
+        {
+            // Setup
+            this.farm = new Farm()
+            // this.fox = new Fox()
+            this.environment = new Environment()
+        })
+    }
+
+    update()
+    {
+        if(this.fox)
+            this.fox.update()
+    }
+}
